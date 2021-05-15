13d3938113f6a720f2d60ba89451f42015f8105d
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2021-05-15 19:01:48 +0300

    refactor params

--- GraphicsTestApp/Source/UIKit/ChartView.swift
+++ GraphicsTestApp/Source/UIKit/ChartView.swift
@@ -38,12 +38,17 @@ public class ChartView: UIView {
         var currentYLinePoint: CGFloat = 0
         var numberOfLine = 0
         while currentYLinePoint <= heightOfChart {
-            chartPath.move(to: CGPoint(x:0, y:currentYLinePoint))
+            chartPath.move(to: CGPoint(x:0,
+                                       y:currentYLinePoint))
             if numberOfLine != 0 {
-                let yValue = setValueForYRow(currentPoint: chartPath.currentPoint, heightOfGraph: heightOfChart, rect: rect, ySpacing: ySpacing)
-                addYLabel(point: chartPath.currentPoint, value: yValue)
+                let yValue = setValueForYRow(currentPoint: chartPath.currentPoint,
+                                             heightOfGraph: heightOfChart,
+                                             rect: rect, ySpacing: ySpacing)
+                addYLabel(point: chartPath.currentPoint,
+                          value: yValue)
             }
-            chartPath.addLine(to: CGPoint(x: rect.width, y: currentYLinePoint))
+            chartPath.addLine(to: CGPoint(x: rect.width,
+                                          y: currentYLinePoint))
             currentYLinePoint = currentYLinePoint + heightOfChart / 5
             numberOfLine += 1
         }
@@ -95,22 +100,30 @@ public class ChartView: UIView {
     }
     
     private func addYLabel(point: CGPoint, value: String) {
-        let yLabel = UILabel(frame: CGRect(x: point.x + 5, y: point.y - 20, width: 100, height: 20))
+        let yLabel = UILabel(frame: CGRect(x: point.x + 5,
+                                           y: point.y - 20,
+                                           width: 100,
+                                           height: 20))
         self.addSubview(yLabel)
         yLabel.textColor = .lightGray
         yLabel.text = value
     }
     
     private func addXLabel(point: CGPoint, value: String) {
-        let xLabel = UILabel(frame: CGRect(x: point.x, y: point.y + 5, width: 100, height: 20))
+        let xLabel = UILabel(frame: CGRect(x: point.x,
+                                           y: point.y + 5,
+                                           width: 100,
+                                           height: 20))
         self.addSubview(xLabel)
         xLabel.textColor = .lightGray
         xLabel.text = value
     }
     
-    private func setValueForYRow(currentPoint: CGPoint, heightOfGraph: CGFloat, rect: CGRect, ySpacing: CGFloat) -> String {
+    private func setValueForYRow(currentPoint: CGPoint,
+                                 heightOfGraph: CGFloat,
+                                 rect: CGRect,
+                                 ySpacing: CGFloat) -> String {
         var allYValues: [Int] = []
-        
         let numberOfLines = (graphData?.y?.count ?? 1) - 1
         for y in 0...numberOfLines {
             let numberOfYPoints = (graphData?.y?[y].count ?? 1) - 1
